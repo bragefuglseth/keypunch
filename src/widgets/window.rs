58f070f1610e7e6752d485d729c22f92794d009a
Brage Fuglseth <bragefuglseth@gmail.com>
2024-05-13 15:37:21 +0200

    window: Set language to English again

--- src/widgets/window.rs
+++ src/widgets/window.rs
@@ -114,7 +114,7 @@ mod imp {
     impl ObjectImpl for KpWindow {
         fn constructed(&self) {
             self.parent_constructed();
-            self.language.set(Language::Spanish);
+            self.language.set(Language::EnglishUS);
 
             self.load_settings();
             self.setup_session_config();
