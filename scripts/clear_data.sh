881f6b9787a8c94bc6b697549f6753d16ec0726c
Brage Fuglseth <brage.fuglseth@gmail.com>
2024-05-27 06:46:06 +0200

    scripts: Correct file name

--- scripts/clear_data.sh
+++ scripts/clear_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -rf ~/.var/app/dev.bragefuglseth.Keypunch
-echo "Cleared ~/.var/app/dev.bragefuglseth.Keypunch"
+rm -rf ~/.var/app/dev.bragefuglseth.Keypunch.Devel
+echo "Cleared ~/.var/app/dev.bragefuglseth.Keypunch.Devel"
