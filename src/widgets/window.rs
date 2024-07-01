c2968a8c3d771c722d67e3f60e1f870419db1592
Brage Fuglseth <brage.fuglseth@gmail.com>
2024-07-01 14:32:24 +0200

    project: Bump version

--- src/widgets/window.rs
+++ src/widgets/window.rs
@@ -202,7 +202,7 @@ mod imp {
 
             let about = adw::AboutDialog::from_appdata(
                 "/dev/bragefuglseth/Keypunch/dev.bragefuglseth.Keypunch.metainfo.xml",
-                Some("1.0"),
+                Some("2.0"),
             );
 
             about.set_developers(&["Brage Fuglseth https://bragefuglseth.dev"]);
