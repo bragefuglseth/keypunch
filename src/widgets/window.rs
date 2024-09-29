4d285ae217214f68b1c88f665c1aeaf9a4ef7993
Brage Fuglseth <brage.fuglseth@gmail.com>
2024-09-29 19:02:50 +0200

    window: Add other apps to about

--- src/widgets/window.rs
+++ src/widgets/window.rs
@@ -246,6 +246,15 @@ mod imp {
                 &["Sophie Herold https://www.patreon.com/sophieh"],
             );
 
+            about.add_other_app(
+                "dev.bragefuglseth.Fretboard",
+                "Fretboard",
+                // Translators: The summary of the app Fretboard. This should match the translated
+                // version of Fretboard's actual summary, if it has been translated into your language.
+                // Have a look at <https://github.com/bragefuglseth/fretboard>
+                &gettext("Look up guitar chords"),
+            );
+
             about.connect_closed(glib::clone!(
                 #[weak(rename_to = imp)]
                 self,
