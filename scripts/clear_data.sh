ec811106ae727f80798269194dd72b3cc1416222
Brage Fuglseth <brage.fuglseth@gmail.com>
2026-07-17 23:56:09 +0200

    fix: Switch app ID in last few places

--- scripts/clear_data.sh
+++ scripts/clear_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -rf ~/.var/app/dev.bragefuglseth.Keypunch.Devel
-echo "Cleared ~/.var/app/dev.bragefuglseth.Keypunch.Devel"
+rm -rf ~/.var/app/no.bragefuglseth.Keypunch.Devel
+echo "Cleared ~/.var/app/no.bragefuglseth.Keypunch.Devel"
